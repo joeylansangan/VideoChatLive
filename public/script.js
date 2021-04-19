59b176acea564fa9b0e7f6fada8296813ed92a65
Joey Lansangan <joeylansangan@gmail.com>
2021-04-19 12:24:02 -0500

    peerjs unique user id generated/ video stream append

--- public/script.js
+++ public/script.js
@@ -1,3 +1,32 @@
 const socket = io('/');
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
 
-socket.emit('join-room', ROOM_ID, 10);
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream)
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+})
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
