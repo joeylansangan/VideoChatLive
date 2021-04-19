8d867ddf720617a6a819a3d8548087b7c1560bf1
Joey Lansangan <joeylansangan@gmail.com>
2021-04-19 12:51:22 -0500

    multiple user stream rendering

--- public/script.js
+++ public/script.js
@@ -14,6 +14,16 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream => {
     addVideoStream(myVideo, stream)
 
+    myPeer.on('call', call => {
+        call.answer(stream)
+
+        const video = document.createElement('video')
+
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
     socket.on('user-connected', userId => {
         connectToNewUser(userId, stream)
     })
@@ -22,6 +32,17 @@ navigator.mediaDevices.getUserMedia({
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);
 })
+
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream) 
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+}
 
 function addVideoStream(video, stream) {
     video.srcObject = stream
