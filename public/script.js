5113b0e3a5311821212da6d7ea97bd114c1c8227
Joey Lansangan <joeylansangan@gmail.com>
2021-04-19 13:32:34 -0500

    added heroku peer server

--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid')
+
 const myPeer = new Peer(undefined, {
-    host: '/',
-    port: '3001'
+    host: 'videopeerserver.herokuapp.com/',
+    secure: true
+    // port: `3001`
 })
 
 const myVideo = document.createElement('video');
