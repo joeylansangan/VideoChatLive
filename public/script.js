21fe70f63b86bdbeed863ba419648683b8f1029d
Joey Lansangan <joeylansangan@gmail.com>
2021-04-19 13:46:01 -0500

    peer server test:

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const socket = io('/');
 const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer(undefined, {
-    host: 'videopeerserver.herokuapp.com/',
+    host: 'videopeerserver.herokuapp.com',
     secure: true,
     port: '3001'
 })
